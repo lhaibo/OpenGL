9d27b44ebae7033bd181d43036ec8cd76b3ecae5
lhaibo <1170946850@qq.com>
2020-06-04 20:29:18 +0800

    成功创建三角形!

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -11,6 +11,28 @@
 const GLuint WIDTH = 800, HEIGHT = 600;
 void processInput(GLFWwindow* window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+//数据
+float vertices[] = {
+	-0.5f,-0.5f,0.0f,
+	0.5f,-0.5f,0.0f,
+	0.0f,0.5f,0.0f
+};
+
+//Vertex Shader
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout(location = 0) in vec3 aPos;\n"
+"void main(){\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}\n";
+
+//Fragment Shader
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main(){\n"
+"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}\n";
+
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
@@ -48,34 +70,55 @@ int main()
 	glViewport(0, 0, width, height);//设置opengl渲染窗口的尺寸大小
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//绑定回调函数
 
-	//数据
-	float vertices[] = {
-		-0.5f,-0.5f,0.0f,
-		0.5f,-0.5f,0.0f,
-		0.5f,0.5f,0.0f
-	};
+	//
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
 	////实例化顶点缓冲对象
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);//将顶点缓冲对象绑定到GL_ARRAY_BUFFER
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);//把数据复制到当前绑定缓冲
-	////创建顶点着色器
-	//unsigned int vertexShader;
-	//vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	//glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	//glCompileShader(vertexShader);
-	// Game loop
+	
+	//创建顶点着色器
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//创建片面着色器
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//创建着色器程序
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Game loop 游戏循环
 	while (!glfwWindowShouldClose(window))//渲染循环
 	{
 		//输入
 		processInput(window);//在每一帧中检测
 
-		// 渲染指令
-		//。。。。。。
 		//状态设置函数
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);//用一种颜色清空屏幕
 		//状态使用函数
 		glClear(GL_COLOR_BUFFER_BIT);//清空颜色缓冲
+
+		// 渲染指令
+		glBindVertexArray(VAO);
+		glUseProgram(shaderProgram);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//检测并调用事件
 		glfwPollEvents();
